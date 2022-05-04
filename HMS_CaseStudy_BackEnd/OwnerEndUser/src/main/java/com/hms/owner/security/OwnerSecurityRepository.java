7bfd0ccf72f73fea5ee21033e73e0ab4153ec5b6
Rajesh <chagantipati.rajesh09@gmail.com>
2022-05-04 07:04:09 +0530

    Backend cleared

--- HMS_CaseStudy_BackEnd/OwnerEndUser/src/main/java/com/hms/owner/security/OwnerSecurityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hms.owner.security;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnerSecurityRepository extends MongoRepository<OwnerSecurityModel, String> {
-
-}
--- HMS_CaseStudy_BackEnd/ReceptionistEndUser/src/main/java/com/hms/receptionist/security/ReceptionistSecurityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hms.receptionist.security;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReceptionistSecurityRepository extends MongoRepository<ReceptionistSecurityModel, String> {
-
-}
